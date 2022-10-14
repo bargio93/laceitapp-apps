c7ffee9069b2c352400bd17ecaa64e135891aa40
bargio93 <barbera.giovanni93@gmail.com>
2022-10-14 14:46:32 +0200

    Add permissions Remove Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    firebaseFunction();
+    //firebaseFunction();
     var status = Permissions.checkServiceStatus(context);
     print(status);
     return  OverlaySupport.global(child: MaterialApp(
